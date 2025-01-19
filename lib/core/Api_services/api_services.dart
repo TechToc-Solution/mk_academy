9c50e1932485d51cb7dcf25995c6e4ad77040d17
ayhamkefo <ayhamkefo048@gmail.com>
2025-01-19 19:58:54 +0300

    add api services , assets , styels class

--- lib/core/Api_services/api_services.dart
+++ lib/core/Api_services/api_services.dart
@@ -1,13 +1,40 @@
 import 'package:dio/dio.dart';
+import 'package:mk_academy/core/Api_services/urls.dart';
+
+import '../utils/cache_helper.dart';
+import '../utils/constats.dart';
 
 class ApiServices {
-  final _basUrl = 'https://www.googleapis.com/books/v1/';
   final Dio _dio;
-  ApiServices(
-    this._dio,
-  );
-  Future<Map<String, dynamic>> get({required endPoint}) async {
-    var response = await _dio.get('$_basUrl$endPoint');
-    return response.data;
+  ApiServices(this._dio) {
+    _dio.options.baseUrl = Urls.baseUrl;
+  }
+
+  Map<String, String> _headers() {
+    return {
+      'Authorization': "Bearer ${CacheHelper.getData(key: 'token')}",
+      'Content-Type': 'application/json',
+      "Accept": 'application/json',
+      "Accept-Charset": "application/json",
+      "Accept-Language": lang,
+    };
+  }
+
+  Future<Response> get({required String endPoint}) {
+    return _dio.get(endPoint, options: Options(headers: _headers()));
+  }
+
+  Future<Response> post({required String endPoint, required dynamic data}) {
+    return _dio.post(endPoint,
+        data: data, options: Options(headers: _headers()));
+  }
+
+  Future<Response> put({required String endPoint, required dynamic data}) {
+    return _dio.put(endPoint,
+        data: data, options: Options(headers: _headers()));
+  }
+
+  Future<Response> delete({required String endPoint}) {
+    return _dio.delete(endPoint, options: Options(headers: _headers()));
   }
 }
---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const textStyle20 =
+      TextStyle(fontSize: 20, fontWeight: FontWeight.normal);
+  static const textStyle18 =
+      TextStyle(fontSize: 18, fontWeight: FontWeight.normal);
+  static const textStyle16 =
+      TextStyle(fontSize: 16, fontWeight: FontWeight.normal);
+  static const textStyle15 =
+      TextStyle(fontSize: 15, fontWeight: FontWeight.normal);
+  static const textStyle13 =
+      TextStyle(fontSize: 13, fontWeight: FontWeight.normal);
+}
