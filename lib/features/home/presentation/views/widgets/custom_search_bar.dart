d1c0f218256c09cbe57b94f674929e64ef21d48c
AHNXD <nzamlyalhady@gmail.com>
2025-01-24 22:34:54 +0400

    fix search bar

--- lib/features/home/presentation/views/widgets/custom_search_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_search_bar.dart
@@ -25,7 +25,7 @@ class CustomSearchBar extends StatelessWidget {
         Expanded(
           child: Container(
             height: 50,
-            padding: EdgeInsets.all(16),
+            padding: EdgeInsets.only(top: 12, right: 16, left: 18, bottom: 16),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(8),
               border: Border.all(color: AppColors.primaryColors),
@@ -36,10 +36,18 @@ class CustomSearchBar extends StatelessWidget {
                   Icons.search,
                   color: Colors.white,
                 ),
+                SizedBox(
+                  width: 4,
+                ),
                 Text(
                   "search".tr(context),
                   style: TextStyle(color: Colors.white),
                 ),
+                Spacer(),
+                Icon(
+                  Icons.favorite_border,
+                  color: Colors.white,
+                )
               ],
             ),
           ),
